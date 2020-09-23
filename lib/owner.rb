de24f3f6056c7344de1605bec8a62d45adb033b7
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 15:53:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,11 +45,7 @@ class Owner
   end
 
   def sell_pets
-    self.cats.each do |cat|
-      self.cats.delete(cat)
-      cat.owner = nil
-      cat.mood = "nervous"
-    end
+    
   end 
   
   def list_pets 
