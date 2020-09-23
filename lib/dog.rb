e5751e4363315a3f4b822801c423ff1c2ac7dd12
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 15:43:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
-  attr_accessor :mood 
+  attr_accessor :mood, :owner 
   attr_reader :name 
+  @@all = []
   
   def initialize(name, mood)
     @name = name 
     @mood = "nervous" 
+    @owner = owner 
+    @@all << self 
+    owner.dogs << self 
   end 
 end
